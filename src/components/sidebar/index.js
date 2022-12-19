aec898af3afb4d7152fabf908849bfdf1f385e59
Kulyomin <96087433+Kulyomin@users.noreply.github.com>
2022-12-20 02:35:29 +0300

    #1 Music is working
    Не хватает запуска рекомендуемых плейлистов.

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import "./sidebar.css";
+import SidebarButton from "./sidebarButton";
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
+import logo from "../../assets/logo.png";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar-container">
+      <img width="150px" height="150px" src={logo} alt="" />
+      <div>
+        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
+        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
+        <SidebarButton
+          title="Favorites"
+          to="/favorites"
+          icon={<MdFavorite />}
+        />
+        <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
+      </div>
+      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
+    </div>
+  );
+}
---
+++ src/components/widgets/widgetCard.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./widgetCard.css";
+import WidgetEntry from "./widgetEntry";
+import { IconContext } from "react-icons";
+import { FiChevronRight } from "react-icons/fi";
+
+export default function WidgetCard({ title, similar, featured, newRelease }) {
+   return (
+      <div className="widgetcard-body">
+         <p className="widget-title">{title}</p>
+         {similar
+            ? similar.map((artist) => (
+               <WidgetEntry
+                  key={artist?.id}
+                  title={artist?.name}
+                  subtitle={artist?.followers?.total + " Followers"}
+                  image={artist?.images[2]?.url}
+               />
+            ))
+            : featured
+               ? featured.map((playlist) => (
+                  <WidgetEntry
+                     key={playlist?.id}
+                     title={playlist?.name}
+                     subtitle={playlist?.tracks?.total + " Songs"}
+                     image={playlist?.images[0]?.url}
+                  />
+               ))
+               : newRelease
+                  ? newRelease.map((album) => (
+                     <WidgetEntry
+                        key={album?.id}
+                        title={album?.name}
+                        subtitle={album?.artists[0]?.name}
+                        image={album?.images[2]?.url}
+                     />
+                  ))
+                  : null}
+         <div className="widget-fade">
+            <div className="fade-button">
+               <IconContext.Provider value={{ size: "24px", color: "#c4d0e3" }}>
+                  <FiChevronRight />
+               </IconContext.Provider>
+            </div>
+         </div>
+      </div>
+   );
+}
